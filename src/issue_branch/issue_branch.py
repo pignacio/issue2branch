5a20d9d27d537b375896c27b3ceafbd53855b7f4
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-08 23:42:27 -0300

    Bitbucket tracker class

--- src/issue_branch/issue_branch.py
+++ src/issue_branch/issue_branch.py
@@ -84,9 +84,24 @@ class Github(IssueTracker):
     def from_remotes(cls, remotes):
         return cls._from_remotes(remotes, domain_has='github.com')
 
+class Bitbucket(IssueTracker):
+    def _get_issue_title(self, contents):
+        div = contents.find(id='issue-view')
+        issue_id = div.find('span', "issue-id").text
+        title = div.find(id='issue-title').text
+        return "{} {}".format(issue_id, title)
+
+    @classmethod
+    def from_remotes(cls, remotes):
+        return cls._from_remotes(remotes, domain_has='bitbucket.org')
+
+    def _get_issue_url(self, issue):
+        return "{}/issue/{}".format(self._base_url, issue)
+
 ISSUE_TRACKERS = {
     'redmine' : Redmine,
     'github' : Github,
+    'bitbucket' : Bitbucket,
 }
 
 def _get_git_root():
