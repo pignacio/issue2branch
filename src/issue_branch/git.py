4141f8cb7d7bf16e34fb481f4548e278bfe0523b
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-17 18:02:02 -0300

    Extract git._run_command method

--- src/issue_branch/git.py
+++ src/issue_branch/git.py
@@ -6,10 +6,15 @@ Created on May 17, 2014
 import subprocess
 
 def get_git_root():
-    command = ['git', 'rev-parse', '--show-toplevel']
+    command = ['rev-parse', '--show-toplevel']
+    proc = _run_command(command)
+    return proc.stdout.readline().strip()
+
+def _run_command(command):
+    command = ["git"] + command
     proc = subprocess.Popen(command, stdout=subprocess.PIPE)
     proc.wait()
 
     if proc.poll():
-        raise ValueError("Could not get git root directory. Is this a git repo?")
-    return proc.stdout.readline().strip()
+        raise ValueError("Command {} failed. Is this a git repo?".format(command))
+    return proc
