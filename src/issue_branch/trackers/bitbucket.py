223ebaaebe191b1fe90378f1a100c535b256851e
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-17 18:47:01 -0300

    Implement issue list for bitbucket

--- src/issue_branch/trackers/bitbucket.py
+++ src/issue_branch/trackers/bitbucket.py
@@ -4,6 +4,7 @@ Created on May 17, 2014
 @author: ignacio
 '''
 from trackers.base import IssueTracker
+import requests
 
 
 class Bitbucket(IssueTracker):
@@ -20,3 +21,25 @@ class Bitbucket(IssueTracker):
 
     def _get_issue_url(self, issue):
         return "{}/issue/{}".format(self._base_url, issue)
+
+    def get_issues(self):
+        if not (self._repo_user and self._repo_name):
+            raise ValueError("Could not parse repo and user from url '{}'"
+                             .format(self._base_url))
+        issues = _api_get("repositories/{}/{}/issues".format(self._repo_user,
+                                                      self._repo_name))
+        return {issue['local_id']: issue['title'] for issue in issues['issues']}
+
+
+def _api_url(path):
+    return "https://bitbucket.org/api/1.0/{}".format(path)
+
+def _api_get(path):
+    url = _api_url(path)
+    response = requests.get(url)
+    if not response.status_code == 200:
+        raise ValueError("bitbucket api returned code {} != 200 for '{}'"
+                         .format(response.status_code, url))
+    return response.json()
+
+
