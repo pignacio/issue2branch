3fa7ae1cec7054568dd7540b3b65cc8200b1f324
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-17 18:38:11 -0300

    Implement issue list for github

--- src/issue_branch/trackers/github.py
+++ src/issue_branch/trackers/github.py
@@ -4,6 +4,8 @@ Created on May 17, 2014
 @author: ignacio
 '''
 from trackers.base import IssueTracker
+import requests
+import re
 
 
 class Github(IssueTracker):
@@ -14,6 +16,28 @@ class Github(IssueTracker):
                 " ".join(div.find('span', c).text
                          for c in ['gh-header-number', 'js-issue-title']))
 
+    def get_issues(self):
+        if not (self._repo_user and self._repo_name):
+            raise ValueError("Could not parse repo and user from url '{}'"
+                             .format(self._base_url))
+        issues = _api_get("repos/{}/{}/issues".format(self._repo_user,
+                                                      self._repo_name))
+        return {issue['number']: issue['title'] for issue in issues}
+
     @classmethod
     def from_remotes(cls, remotes):
         return cls._from_remotes(remotes, domain_has='github.com')
+
+
+def _api_url(path):
+    return "https://api.github.com/{}".format(path)
+
+def _api_get(path):
+    url = _api_url(path)
+    response = requests.get(url)
+    if not response.status_code == 200:
+        raise ValueError("Github api returned code {} != 200 for '{}'"
+                         .format(response.status_code, url))
+    return response.json()
+
+
