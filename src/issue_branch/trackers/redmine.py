b795c92331bbe9af318309c374c0999b019f544d
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-05-19 10:29:59 -0300

    Using redmine API to get issue title

--- src/issue_branch/trackers/redmine.py
+++ src/issue_branch/trackers/redmine.py
@@ -8,6 +8,10 @@ from trackers.base import IssueTracker
 
 class Redmine(IssueTracker):
 
+    def _get_issue_url(self, issue):
+        return IssueTracker._get_issue_url(self, issue) + ".json"
+
     def _get_issue_title(self, contents):
-        content_div = contents.find(id='content')
-        return "{} {}".format(content_div.h2.text, content_div.h3.text)
+        issue = contents['issue']
+        tracker = issue['tracker']['name']
+        return "{} {} {}".format(tracker, issue['id'], issue['subject'])
