e8104674be2d1fdda2245c992333d73219cff6a8
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-06-04 21:51:11 -0300

    Removed pylint warnings in color module

--- src/color.py
+++ src/color.py
@@ -29,11 +29,13 @@ def _color_func_pair(color):
     return _color_func(color), _color_func(color + BRIGHT)
 
 
-black, bright_black = _color_func_pair(BLACK)
-red, bright_red = _color_func_pair(RED)
-green, bright_green = _color_func_pair(GREEN)
-yellow, bright_yellow = _color_func_pair(YELLOW)
-blue, bright_blue = _color_func_pair(BLUE)
-magenta, bright_magenta = _color_func_pair(MAGENTA)
-cyan, bright_cyan = _color_func_pair(CYAN)
-white, bright_white = _color_func_pair(WHITE)
+black, bright_black = _color_func_pair(BLACK)  # pylint: disable=invalid-name
+red, bright_red = _color_func_pair(RED)  # pylint: disable=invalid-name
+green, bright_green = _color_func_pair(GREEN)  # pylint: disable=invalid-name
+(yellow,  # pylint: disable=invalid-name
+ bright_yellow) = _color_func_pair(YELLOW)  # pylint: disable=invalid-name
+blue, bright_blue = _color_func_pair(BLUE)  # pylint: disable=invalid-name
+(magenta,  # pylint: disable=invalid-name
+ bright_magenta) = _color_func_pair(MAGENTA)  # pylint: disable=invalid-name
+cyan, bright_cyan = _color_func_pair(CYAN)  # pylint: disable=invalid-name
+white, bright_white = _color_func_pair(WHITE)  # pylint: disable=invalid-name
