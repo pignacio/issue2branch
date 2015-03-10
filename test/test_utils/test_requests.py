63b26748f65335568552b28a105dd960868af409
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-03-10 16:42:53 -0300

    New request tests

--- test/test_utils/test_requests.py
+++ test/test_utils/test_requests.py
@@ -5,13 +5,12 @@ from __future__ import absolute_import, unicode_literals
 from unittest import TestCase
 import logging
 
-from mock import Mock, create_autospec, patch
+from mock import create_autospec, patch, sentinel
 from nose.tools import eq_, raises
 import requests
 
 from issue2branch.utils.requests import (
     request, get_response_content, NotOkResponse)
-from ..utils import Sentinels
 
 
 
@@ -21,47 +20,52 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 class TestRequest(TestCase):
     ''' Tests for `issue2branch.utils.requests.request`. '''
     def setUp(self):
-        self.sentinels = Sentinels()
-        self.method = Mock()
+        self.method = create_autospec(requests.get)
         self.response = create_autospec(requests.Response, status_code=200)
         self.method.return_value = self.response
 
     def test_no_user_proxy(self):
-        response = request(self.method, self.sentinels.url,
-                           kwarg=self.sentinels.kwvalue)
-        self.method.assert_called_once_with(self.sentinels.url,
-                                            kwarg=self.sentinels.kwvalue)
+        response = request(self.method, sentinel.url,
+                           kwarg=sentinel.kwvalue)
+        self.method.assert_called_once_with(sentinel.url,
+                                            kwarg=sentinel.kwvalue)
         eq_(response, self.response)
 
 
     def test_user_proxy(self):
-        response = request(self.method, self.sentinels.url,
-                           self.sentinels.username, self.sentinels.password,
-                           kwarg=self.sentinels.kwvalue)
-        self.method.assert_called_once_with(self.sentinels.url,
-                                            auth=(self.sentinels.username,
-                                                  self.sentinels.password),
-                                            kwarg=self.sentinels.kwvalue)
+        request(self.method, sentinel.url,
+                sentinel.username, sentinel.password,
+                kwarg=sentinel.kwvalue)
+        self.method.assert_called_once_with(sentinel.url,
+                                            auth=(sentinel.username,
+                                                  sentinel.password),
+                                            kwarg=sentinel.kwvalue)
 
     @raises(NotOkResponse)
     def test_not_found_fails(self):
         self.response.status_code = 404
-        request(self.method, self.sentinels.url)
+        request(self.method, sentinel.url)
 
     @raises(NotOkResponse)
     def test_server_error_fails(self):
         self.response.status_code = 500
-        request(self.method, self.sentinels.url)
+        request(self.method, sentinel.url)
+
+    def test_exception_contains_response(self):
+        self.response.status_code = 500
+        try:
+            request(self.method, sentinel.url)
+        except NotOkResponse as err:
+            eq_(err.response, self.response)
 
 
 class TestGetResponseContent(TestCase):
     ''' Tests for `issue2branch.utils.requests.get_response_content`. '''
     def setUp(self):
-        self.sentinels = Sentinels()
         self.response = create_autospec(requests.Response)
 
     def test_parses_json(self):
-        self.response.json.return_value = self.sentinels.json
+        self.response.json.return_value = sentinel.json
         self.response.headers = {
             'content-type': 'application/json',
         }
@@ -69,26 +73,26 @@ class TestGetResponseContent(TestCase):
         content = get_response_content(self.response)
 
         self.response.json.assert_called_once_with()
-        eq_(content, self.sentinels.json)
+        eq_(content, sentinel.json)
 
     @patch('issue2branch.utils.requests.BeautifulSoup')
     def test_parses_html(self, mock_soup):
-        self.response.content = self.sentinels.content
+        self.response.content = sentinel.content
         self.response.headers = {
             'content-type': 'text/html',
         }
-        mock_soup.return_value = self.sentinels.soup
+        mock_soup.return_value = sentinel.soup
 
         content = get_response_content(self.response)
 
-        mock_soup.assert_called_once_with(self.sentinels.content)
-        eq_(content, self.sentinels.soup)
+        mock_soup.assert_called_once_with(sentinel.content)
+        eq_(content, sentinel.soup)
 
     def test_parses_plain(self):
-        self.response.content = self.sentinels.content
+        self.response.content = sentinel.content
         self.response.headers = {
             'content-type': 'text/plain',
         }
 
         content = get_response_content(self.response)
-        eq_(content, self.sentinels.content)
+        eq_(content, sentinel.content)
--- test/utils.py
+++ test/utils.py
@@ -2,35 +2,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+from ConfigParser import SafeConfigParser
+from StringIO import StringIO
+import collections
 import logging
+
+from issue2branch.config import Config
 
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-class _Sentinel(object):  # pylint: disable=too-few-public-methods
-    def __init__(self, parent, name):
-        self._parent = parent
-        self._name = name
-
-    def __repr__(self):
-        return "Sentinel({id}):{parent} @ {name}".format(
-            id=hex(id(self)), parent=hex(id(self._parent)), name=self._name)
+def config_from_string(value):
+    config = SafeConfigParser()
+    config.readfp(StringIO(value))
+    return Config(config)
 
 
-class Sentinels(object):  # pylint: disable=too-few-public-methods
-    def __init__(self):
-        self._sentinels = {}
+def mock_properties(**values):
+    return collections.namedtuple('MockProperties', list(values))(**values)
 
-    def __getattribute__(self, attr):
-        if attr.startswith('_'):
-            return super(Sentinels, self).__getattribute__(attr)
-        return self.__getitem__(attr)
-
-    def __getitem__(self, key):
-        try:
-            return self._sentinels[key]
-        except KeyError:
-            sentinel = _Sentinel(self, key)
-            self._sentinels[key] = sentinel
-            return sentinel
