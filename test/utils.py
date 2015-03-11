9c8eb4da1962558ee5d3dcdbab91f622f5db143c
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-03-11 14:32:19 -0300

    tests: namedtuple mocks

--- test/utils.py
+++ test/utils.py
@@ -35,3 +35,37 @@ class TestCase(UnitTestCase):
         patcher = patch.object(*args, **kwargs)
         self.addCleanup(patcher.stop)
         return patcher.start()
+
+
+def mock_namedtuple_class(tuple_class):
+    class MockTuple(tuple_class):  # pylint: disable=too-few-public-methods
+        __EXCEPTION_SENTINEL = object()
+        def __new__(cls, **kwargs):
+            values = [kwargs.get(f, cls.__EXCEPTION_SENTINEL)
+                      for f in tuple_class._fields]
+            return tuple_class.__new__(cls, *values) # pylint: disable=star-args
+
+        def __init__(self, **kwargs):
+            for field in kwargs:
+                if not field in self._fields:
+                    raise ValueError("'{}' is not a valid field for {}".format(
+                        field, tuple_class))
+            kwargs = {f: kwargs.get(f, self.__EXCEPTION_SENTINEL)
+                      for f in self._fields}
+            tuple_class.__init__(self, **kwargs)
+
+        def __getattribute__(self, attr):
+            # Avoid recursion filtering _* lookups without doing self._* lookups
+            value = tuple_class.__getattribute__(self, attr)
+            if attr.startswith("_"):
+                return value
+            if value == self.__EXCEPTION_SENTINEL:
+                raise AttributeError("Missing '{}' field in '{}' mock. (id={})"
+                                     .format(attr, tuple_class.__name__,
+                                             id(self)))
+            return value
+    return MockTuple
+
+
+def mock_namedtuple(tuple_class, **kwargs):
+    return mock_namedtuple_class(tuple_class)(**kwargs)
