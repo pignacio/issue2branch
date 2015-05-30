373c49a14210ce8bdc186284d9ca3034a83affea
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-05-30 13:14:52 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.7',
+    version='0.0.8',
 
     description='A feature branch namer / Issue tracker manager',
     long_description=long_description,
